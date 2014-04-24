744268dd28a91fd313047c2680e918e7b70240c9
beechertrouble <beechertrouble@gmail.com>
2014-04-24 10:49:14 -0400

    more AMD, added min

--- pickyImg.js
+++ pickyImg.js
@@ -5,200 +5,226 @@
 * v0.0.1
 * needs jQuery, and maybe UAjammer to work ...
 */
-var _pickyImg = (function _pickyImg(W, $) {
+;(function($, W) {
+
+	'use strict';
+
+	/**
+	 * for AMD, don't redefine this!  (need to maintain globals and plugins)
+	 */
+	if (W._pickyImg)
+		return;
 	
-	var ME = this,
-		UA = W._UAjammer !== undefined ? W._UAjammer : undefined,
-		args,
-		pad,
-		wChangeTimer,
-		wIsStopped = true,
-		whichSrc,
-		srcMap,
-		selector,
-		origin = {},
-		callbacks = {},
-		bindMe,
-		// methods
-		binder, wStopped, pickMe, isFinished, defMap
-		;
-	
-	ME.doCallback = function(event) {
-				
-		var func = callbacks[event];
-			
-		if(func !== undefined && typeof callbacks[event] == 'function')
-			func();
-			
-	};
-	
-	// ....
-			
-	ME.init = function(initArgs) {
-	
-		/* inView plugin :: https://github.com/beechertrouble/inView */
-		if(!$().inView){$.fn.inView=function(j){var h=false,g=$(window);if(this.length>0){var i=this.offset()===null?0:this.offset().top,f=i+this.height();j=j===undefined?g.height():j;h=((g.scrollTop()+g.height())+j)>=i&&(g.scrollTop()-j)<=f?true:false}return h}};
+	$ = $;
 		
-		args = initArgs !== undefined ? initArgs : {};
-		pad = args.pad !== undefined ? args.pad : $(W).height();
-		whichSrc = args.whichSrc !== undefined ? args.whichSrc : 'data-src-default';
-		srcMap = args.srcMap !== undefined ? args.srcMap : undefined;
-		selector = args.selector !== undefined ? args.selector : '._picky';
-		bindMe = args.bindMe !== undefined ? args.bindMe : true;
-		callbacks = {
-			init : args.callbacks.init !== undefined ? args.callbacks.init : null,
-			finishedInit : args.callbacks.finishedInit !== undefined ? args.callbacks.finishedInit : null,
-			picking : args.callbacks.picking !== undefined ? args.callbacks.picking : null,
-			finished : args.callbacks.finished !== undefined ? args.callbacks.finished : null
-		};
-				
-		whichSrc = typeof srcMap == 'function' ? srcMap() : defMap();		
+	var _pickyImg = (function() {
 		
-		ME.doCallback('init');
+		var ME = {},
+			UA = W._UAjammer !== undefined ? W._UAjammer : undefined,
+			args,
+			pad,
+			wChangeTimer,
+			wIsStopped = true,
+			whichSrc,
+			srcMap,
+			selector,
+			origin = {},
+			callbacks = {},
+			bindMe,
+			// methods
+			binder, wStopped, pickMe, isFinished, defMap
+			;
 		
-		if(bindMe)
-			binder();
-			
-		ME.doCallback('finishedInit');
-				
-	};
-	
-	defMap = function() {
-		
-		if(UA === undefined || srcMap === undefined)
-			return whichSrc;
-			
-		var chosenSrc = whichSrc;
-		
-		switch(true) {
-			
-			case(UA.Venue == 'phone'):
-				chosenSrc = UA.Pixels >= 2 ? 'data-src-phonex2' : 'data-src-phone';
-				break;
-			
-			case(UA.Venue == 'tablet'):
-				chosenSrc = UA.Pixels >= 2 ? 'data-src-tabletx2' : 'data-src-tablet';
-				break;
-				
-			case(UA.Venue == 'desktop'):
-				chosenSrc = UA.Pixels >= 2 ? 'data-src-desktopx2' : 'data-src-desktop';
-				break;
-								
-		}
-		
-		return chosenSrc;
-			
-	};
-	
-	binder = function() {
+		ME.doCallback = function(event) {
 					
-		$(W).on('scroll resize', function() {
+			var func = callbacks[event];
+				
+			if(func !== undefined && typeof callbacks[event] == 'function')
+				func();
+				
+		};
+		
+		// ....
+				
+		ME.init = function(initArgs) {
+		
+			/* inView plugin :: https://github.com/beechertrouble/inView */
+			if(!$().inView){$.fn.inView=function(j){var h=false,g=$(window);if(this.length>0){var i=this.offset()===null?0:this.offset().top,f=i+this.height();j=j===undefined?g.height():j;h=((g.scrollTop()+g.height())+j)>=i&&(g.scrollTop()-j)<=f?true:false}return h}};
 			
-			wIsStopped = false;
-			clearTimeout(wChangeTimer);
-			wChangeTimer = setTimeout( wStopped , 150 );
-				
-		});
-		
-		$('body')
-			.on('wStopped', selector + ":not(._picky_picked)", function() {
-													
-				ME.doCallback('picking');
-				
-				$(W).trigger('picky_picking');
-				$('body').removeClass('_picky_finished');
-				pickMe($(this));
-				
-			});
-		
-		// trigger first run ...
-		wStopped();
-				
-	};
-	
-	wStopped = function() {
-		
-		wIsStopped = true;
-		$(W).trigger('wStopped');
-		$('body ' + selector).trigger('wStopped');
-		
-	};
-	
-	pickMe = function(dummy) {
-
-		if(dummy.inView(pad) && !dummy.hasClass('_picky_picking')) {
-						
-			dummy.addClass("_picky_picking");
-							
-			var no_script = dummy.find('noscript'),
-				src = dummy.attr(whichSrc), 
-				img = new Image(), 
-				imgClass = dummy.attr('data-class'),
-				imgAlt = dummy.attr('data-alt'),
-				imgLoad_handler;
-
-			imgLoad_handler = function() {
-				
-				if(dummy.hasClass('_picky_bg')) {
-					dummy.css({'background-image' : 'url(' + src + ')'});
-				} else {
-					dummy.prepend($(this));
-				}
-				
-				if(_UAjammer.Browser.Name == 'msie' && _UAjammer.Browser.Version <= 8) 
-					$(this).height( ($(this).height() * $(this).width() ) / this.width);
-
-				$(this).removeClass('_picky_picking').attr('alt', imgAlt);
-				setTimeout(function(){
-					dummy.removeClass('_picky_picking').addClass('_picky_picked');
-					isFinished();
-				}, 10);
-				
+			args = initArgs !== undefined ? initArgs : {};
+			pad = args.pad !== undefined ? args.pad : $(W).height();
+			whichSrc = args.whichSrc !== undefined ? args.whichSrc : 'data-src-default';
+			srcMap = args.srcMap !== undefined ? args.srcMap : undefined;
+			selector = args.selector !== undefined ? args.selector : '._picky';
+			bindMe = args.bindMe !== undefined ? args.bindMe : true;
+			callbacks = {
+				init : args.callbacks.init !== undefined ? args.callbacks.init : null,
+				finishedInit : args.callbacks.finishedInit !== undefined ? args.callbacks.finishedInit : null,
+				picking : args.callbacks.picking !== undefined ? args.callbacks.picking : null,
+				finished : args.callbacks.finished !== undefined ? args.callbacks.finished : null
 			};
+					
+			whichSrc = typeof srcMap == 'function' ? srcMap() : defMap();		
 			
-			// add the loading classes before we even start ...
-			$(img).addClass(imgClass).addClass('_picky_picking');
-
-			// test to see if there is already an image that is trying to load this src ...
-			if (!origin[src]) {
+			ME.doCallback('init');
+			
+			if(bindMe)
+				binder();
 				
-				// store the hash
-				origin[src] = img;
+			ME.doCallback('finishedInit');
+					
+		};
+		
+		defMap = function() {
+			
+			if(UA === undefined || srcMap === undefined)
+				return whichSrc;
 				
-				// do stuff when it loads
-				img.onload = imgLoad_handler;
-											
-			} else {
+			var chosenSrc = whichSrc;
+			
+			switch(true) {
 				
-				// listen for that first image to load bind that to our current image ...
-				var originimg = $(origin[src]);
+				case(UA.Venue == 'phone'):
+					chosenSrc = UA.Pixels >= 2 ? 'data-src-phonex2' : 'data-src-phone';
+					break;
 				
-				// see if it's loaded already ...
-				if (originimg.hasClass('_picky_picked')) {
-					originimg.on('load', imgLoad_handler.bind(img));
-				// call immediately ...
-				} else {
-					imgLoad_handler.bind(img)();
-				}
+				case(UA.Venue == 'tablet'):
+					chosenSrc = UA.Pixels >= 2 ? 'data-src-tabletx2' : 'data-src-tablet';
+					break;
+					
+				case(UA.Venue == 'desktop'):
+					chosenSrc = UA.Pixels >= 2 ? 'data-src-desktopx2' : 'data-src-desktop';
+					break;
+									
 			}
 			
-			dummy.attr('data-pref-src', src);
-			img.src = 	src;
-		
-		}
-		
-	};
-	
-	isFinished = function() {
+			return chosenSrc;
 				
-		if($(selector + ":not(._picky_picked)").length <= 0) {
-			$('body').addClass('_picky_finished');
-			$(W).trigger('picky_finished');
-			ME.doCallback('finished');
-		}
+		};
 		
-	};	
+		binder = function() {
+						
+			$(W).on('scroll resize', function() {
+				
+				wIsStopped = false;
+				clearTimeout(wChangeTimer);
+				wChangeTimer = setTimeout( wStopped , 150 );
+					
+			});
+			
+			$('body')
+				.on('wStopped', selector + ":not(._picky_picked)", function() {
+														
+					ME.doCallback('picking');
+					
+					$(W).trigger('picky_picking');
+					$('body').removeClass('_picky_finished');
+					pickMe($(this));
+					
+				});
+			
+			// trigger first run ...
+			wStopped();
+					
+		};
+		
+		wStopped = function() {
+			
+			wIsStopped = true;
+			$(W).trigger('wStopped');
+			$('body ' + selector).trigger('wStopped');
+			
+		};
+		
+		pickMe = function(dummy) {
 	
-	return this;
+			if(dummy.inView(pad) && !dummy.hasClass('_picky_picking')) {
+							
+				dummy.addClass("_picky_picking");
+								
+				var no_script = dummy.find('noscript'),
+					src = dummy.attr(whichSrc), 
+					img = new Image(), 
+					imgClass = dummy.attr('data-class'),
+					imgAlt = dummy.attr('data-alt'),
+					imgLoad_handler;
+	
+				imgLoad_handler = function() {
+					
+					if(dummy.hasClass('_picky_bg')) {
+						dummy.css({'background-image' : 'url(' + src + ')'});
+					} else {
+						dummy.prepend($(this));
+					}
+					
+					if(_UAjammer.Browser.Name == 'msie' && _UAjammer.Browser.Version <= 8) 
+						$(this).height( ($(this).height() * $(this).width() ) / this.width);
+	
+					$(this).removeClass('_picky_picking').attr('alt', imgAlt);
+					setTimeout(function(){
+						dummy.removeClass('_picky_picking').addClass('_picky_picked');
+						isFinished();
+					}, 10);
+					
+				};
+				
+				// add the loading classes before we even start ...
+				$(img).addClass(imgClass).addClass('_picky_picking');
+	
+				// test to see if there is already an image that is trying to load this src ...
+				if (!origin[src]) {
+					
+					// store the hash
+					origin[src] = img;
+					
+					// do stuff when it loads
+					img.onload = imgLoad_handler;
+												
+				} else {
+					
+					// listen for that first image to load bind that to our current image ...
+					var originimg = $(origin[src]);
+					
+					// see if it's loaded already ...
+					if (originimg.hasClass('_picky_picked')) {
+						originimg.on('load', imgLoad_handler.bind(img));
+					// call immediately ...
+					} else {
+						imgLoad_handler.bind(img)();
+					}
+				}
+				
+				dummy.attr('data-pref-src', src);
+				img.src = 	src;
+			
+			}
+			
+		};
 		
-}(window, jQuery));
+		isFinished = function() {
+					
+			if($(selector + ":not(._picky_picked)").length <= 0) {
+				$('body').addClass('_picky_finished');
+				$(W).trigger('picky_finished');
+				ME.doCallback('finished');
+			}
+			
+		};	
+		
+		return ME;
+			
+	})();
+	
+	/**
+	 * Apply the ikelos function to the supplied scope (window)
+	 */
+	W._pickyImg = _pickyImg;
+
+})(jQuery || $, window);
+
+/**
+ * Expose fingaFingaz as an AMD
+ */
+if (typeof define === "function") {
+	define("_pickyImg", [], function () { return _pickyImg; } );
+}
